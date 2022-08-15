be62081ac4b2fc813796c8928c7d89a29e04339d
nhut_pham <nhut.pham@ilogic.vn>
2022-08-15 09:19:36 +0700

    init proj

---
+++ src/main/java/com/threading/demo/ThreadingPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.threading.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadingPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadingPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/threading/demo/ThreadingPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threading.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadingPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
